7ea91f78e7f4b97bc1b693d0b10c41cdef894a19
JesperMSS <62060621+JesperMSS@users.noreply.github.com>
2021-12-21 00:20:39 +0100

    migration v1 + db connection added

---
+++ CV-Siten/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CV_Siten.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CV_Siten.Models.DB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CV_Siten.Models.DB";
+        }
+
+        protected override void Seed(CV_Siten.Models.DB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
